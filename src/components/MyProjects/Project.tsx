db43475073d5a96e78c4f3b66aca52ece42642d4
luc <luc.gireaud@gmail.com>
2022-09-11 13:19:59 +0200

    Open modal on image click

--- src/components/MyProjects/Project.tsx
+++ src/components/MyProjects/Project.tsx
@@ -1,4 +1,19 @@
-import { VStack, Image, Grid, Heading, Flex } from "@chakra-ui/react";
+import {
+  VStack,
+  Image,
+  Grid,
+  Heading,
+  Flex,
+  useDisclosure,
+  Button,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/react";
 import { ReactNode, useMemo } from "react";
 
 interface ProjectProps {
@@ -11,23 +26,38 @@ interface ProjectProps {
 export function Project(props: ProjectProps) {
   const { image, title, children, reversed } = props;
 
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   const ImageRender = useMemo(
-    () => <Image src={image} rounded="2xl" />,
+    () => <Image src={image} rounded="2xl" onClick={onOpen} cursor="pointer" />,
     [image]
   );
 
   return (
-    <Grid templateColumns="repeat(2, 1fr)">
-      {reversed && ImageRender}
-      <Flex flexDir="column" justifyContent="center" textAlign="center" mx={20}>
-        <Heading size="lg" color="black">
-          {title}
-        </Heading>
-        <VStack mt={10} color="gray.500" fontWeight="semibold" spacing={5}>
-          {children}
-        </VStack>
-      </Flex>
-      {!reversed && ImageRender}
-    </Grid>
+    <>
+      <Grid templateColumns="repeat(2, 1fr)">
+        {reversed && ImageRender}
+        <Flex
+          flexDir="column"
+          justifyContent="center"
+          textAlign="center"
+          mx={20}
+        >
+          <Heading size="lg" color="black">
+            {title}
+          </Heading>
+          <VStack mt={10} color="gray.500" fontWeight="semibold" spacing={5}>
+            {children}
+          </VStack>
+        </Flex>
+        {!reversed && ImageRender}
+      </Grid>
+      <Modal isOpen={isOpen} onClose={onClose} isCentered size="6xl">
+        <ModalOverlay />
+        <ModalContent rounded="3xl">
+          <Image src={image} rounded="3xl" onClick={onClose} />
+        </ModalContent>
+      </Modal>
+    </>
   );
 }
